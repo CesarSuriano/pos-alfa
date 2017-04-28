87166b749d05dfd111531a64759aac2a41caa3ee
César Augusto <cesaraugustokg6@hotmail.com>
2017-04-28 15:24:15 -0300

    Isograma

--- Chiquitto-javascript/trabalho/ex2-isograma/isograma.js
+++ Chiquitto-javascript/trabalho/ex2-isograma/isograma.js
@@ -1,6 +1,28 @@
-let inputPalabra = document.getElementById("palavra");
+let inputPalavra = document.getElementById("palavra");
 let lblResultado = document.getElementById("resultado");
 
-let verificaPalavra = () => {
-    
+let clickVerifica = () => {
+    let palavra = inputPalavra.value;
+
+    if(verificaPalavra(palavra)){
+        lblResultado.innerHTML = "É um isograma!" ;
+    } else {
+        lblResultado.innerHTML = "Não é um isograma!";
+    }
+}
+
+let verificaPalavra = (palavra) => {
+    let palavraOriginal = palavra;
+    let palavraQuebrada = Array.from(palavra);
+    let contador = 0;
+    let ocorrencias = [];
+    for(let i of palavraQuebrada){
+        ocorrencias[i] = ocorrencias[i] ? ocorrencias[i] + 1 : 1;
+        contador ++;
+
+        if(ocorrencias[i] > 1) {
+            return true;
+        }
+    }
+    return false;
 }
