3ae76703f5c81b75f6cb91372867e07f7fd1eb5c
César Augusto <cesaraugustokg6@hotmail.com>
2017-06-03 12:23:32 -0300

    Primeiro commit java

---
+++ Java-oo-Munif/OO/src/main/java/com/mycompany/oo/Jogador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.oo;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Jogador extends Personagem{
+
+    public Jogador(String nome, Lugar lugar) {
+        super(nome, lugar);
+    }
+
+    
+    
+}
