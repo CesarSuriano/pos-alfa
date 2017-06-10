c8f7ad984d4b8f20798587c05e2b68f5dcb05799
César Augusto <cesaraugustokg6@hotmail.com>
2017-06-10 10:58:18 -0300

    Começando bootstrap

---
+++ Burnes-bootstrap/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Carsale - Loja de São Lourenço</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Venda de carros">
+        <meta name="keywords" content="fiat, 147">
+
+        <!--facebook og-open-grafics-->
+        <meta property="og:title" content="Car Sale">
+        <meta property="og:locale" content="pt-BR">
+        <meta property="og:description" content="Venda de carros lalala">
+        <meta property="og:image" content="http://pos.professorburnes.com.br/casale/imgs/carsale.jpg">
+        <meta property="og:image:width" content="800">
+        <meta property="og:image:height" content="315">
+
+        <!--twitter cards-->
+        <meta name="twitter:url" content="@cesarsuriano">
+        <meta name="twitter:image" content="http://pos.professorburnes.com.br/casale/imgs/carsale.jpg">
+        <meta name="twitter:title" content="Car Sale">
+
+        <!--javascript-->
+        <script src="js/jquery-3.1.0.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap-inputmask.js"></script>
+        <script src="js/jqBootstrapValidation.js"></script>
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="icon" type="image/png" href="imgs/icone.png">
+
+    </head>
+    <body>
+    
+    </body>
+</html>
