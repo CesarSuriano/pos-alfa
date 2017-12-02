6e51280830c720115c5446efaf8cb12435ba7fa7
César Augusto <cesaraugustokg6@hotmail.com>
2017-12-02 10:05:13 -0200

    Atualizando as parada

---
+++ xamarin/aula 1/App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //var btn = (Button)sender;
+
+            //DisplayAlert("Clicou ", "Você clicou em "+btn.Text, "ok");
+            //Navigation.PushModalAsync(new Page2());
+            Application.Current.MainPage = new NavigationPage(new Page2());
+        }
+    }
+}
